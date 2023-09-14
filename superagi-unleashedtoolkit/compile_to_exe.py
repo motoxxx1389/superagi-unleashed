f20370f197c7328c53828bcd2a95b370c40f3191
mallard4 <mallardlovett@gmail.com>
2023-09-14 17:16:08 -0500

    Update compile_to_exe.py

--- superagi-unleashedtoolkit/compile_to_exe.py
+++ superagi-unleashedtoolkit/compile_to_exe.py
@@ -1,4 +1,20 @@
-from PyInstaller import __main__ as pyi
+from PyInstaller.__main__ import run as pyi_run
 
 def compile_to_exe(input_files, output_file):
-    pyi.run(['--name=%s' % output_file, '--onefile'] + input_files)
+    """
+    Compiles the given input files into an executable using PyInstaller.
+
+    :param input_files: A single file (str) or a list of files to compile.
+    :param output_file: The name of the output executable.
+    """
+    # Ensure input_files is a list
+    if not isinstance(input_files, list):
+        input_files = [input_files]
+
+    # Construct the PyInstaller command
+    cmd = ['--name={}'.format(output_file), '--onefile'] + input_files
+
+    try:
+        pyi_run(cmd)
+    except Exception as e:
+        print(f"Error during compilation: {e}")
